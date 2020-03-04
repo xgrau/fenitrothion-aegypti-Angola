26b59c4d90f81cfa5f074251babec15849140523
xgrau <xavier.graubove@gmail.com>
2020-03-04 10:47:13 +0000

    freebayes

---
+++ results_variation/README.md
@@ -0,0 +1,66 @@
+# Genetic variants from pooled RNA-seq samples
+
+## Mapping & calling
+
+### Requirements
+
+Tools:
+
+* Mapping: `bwa` 0.7.17-r1188 (`bwa mem` aligner)
+* Read processing: `biobambam2` (in `conda`), for marking duplicate reads and sorting.
+* VCF calling: [`freebayes`](https://github.com/ekg/freebayes) v1.3.1-dirty, combined with GNU `parallel`.
+* VCF processing: `vcflib` and (in `conda`).
+* Phasing: `phaseit` (including `extractPIRs`) // NOT USEFUL FOR RNA-seq.
+* Python 3.
+
+### Steps
+
+1. Create folder structure, use longest isoform per transcript from [Vectorbase](https://www.vectorbase.org/downloads?field_organism_taxonomy_tid%5B%5D=367&field_download_file_format_tid=All&field_status_value=Current) and store it in `${dir}/reference` folder:
+
+   ```bash
+   # home directory where you saved your raw data. Should contain an `input` folder with raw reads (fastq)
+   dir="/home/xavi/dades/Transcriptomes/Aedaeg_Angola_19oct18/"
+   # other folders
+   mkdir -p ${dir}/reference_transcripts # save reference fasta here
+   ref="${dir}/reference_transcripts/Aedaeg_long.cds.fasta" # this is the reference
+   mkdir -p ${dir}/alignments_transcripts # where BAM alignments will go
+   nt=10 # number of threads (bwa and freebayes)
+   ```
+
+2. Index reference genome:
+
+   ```bash
+   bwa index ${ref}
+   samtools faidx ${ref}
+   /home/xavi/Programes/gatk-4.1.2.0/gatk CreateSequenceDictionary -R ${ref} -O ${ref%%.fa}.dict
+   ```
+
+3. Map each sample with `bwa` (run separately for each sample in `../data_metadata/samples.list`), mark duplicates and merge runs with `biobambam`. For `freebayes` genotyping, no base recalibration or indel realignment steps are needed ([here why](https://github.com/ekg/freebayes#calling-variants-from-fastq-to-vcf)).
+
+   ```bash
+   s={SAMPLE}
+   echo "### $s ###"
+   bwa mem -M -t ${nt} ${ref} ${dir}/input/Aedaeg_${s}_1.fastq.gz ${dir}/input/Aedaeg_${s}_2.fastq.gz -R "@RG\tID:${s}\tSM:${s}\tPU:nye\tPL:nye\tLB:${s}" | bamsormadup inputformat=sam threads=${nt} tmpfile=${dir}/alignments_transcripts/tmp_$(date +%s) SO=coordinate > ${dir}/alignments_transcripts/${s}.tmp1.bam && mv ${dir}/alignments_transcripts/${s}.tmp1.bam ${dir}/alignments_transcripts/${s}.bam && rm ${dir}/alignments_transcripts/tmp_*
+
+   ```
+
+4. VCF calling with `freebayes`, filters with `bcftools`.
+
+   ```bash
+   bash s01_freebayes_pool_RNA_v02.sh ../data_metadata/samples.list ${nt}
+   ```
+
+5. Convert VCF to ZARR and/or HDF5:
+
+   ```python
+   import allel
+   allel.vcf_to_hdf5('out_recode.vcf.gz', 'out_recode.h5', exclude_fields=['variants/numalt'], overwrite=True)
+   ```
+
+6. Calculate gene-wise genetic differentiation statistics ($F_{ST}$ and $PBS$) for each species using `scikit-allel`, which will be stored in the `differentiation_sps.csv` files:
+
+   ```bash
+   python s02_differentiation_v03.py
+   ```
+
+7. Post-processing in `R`
---
+++ results_variation/s01_freebayes_pool_RNA_v02.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# define sample list
+sample_list=$(cat $1)
+echo $sample_list
+if [ -z "$sample_list" ] ; then
+	echo "NO SAMPLE LIST PROVIDED!"
+	exit
+fi
+
+# num threads
+nt=$2
+if [ -z "$nt" ] ; then
+	nt=8
+fi
+
+# output prefix
+out_prefix=${3}
+if [ -z "${out_prefix}" ] ; then
+	out_prefix="out"
+fi
+
+
+# directories
+dir="/home/xavi/dades/Transcriptomes/Anogam_pmres_2020-01-09"
+ref="${dir}/reference_transcripts/Anogam_long.cds.fasta"
+generate_regions_py="scripts/fasta_generate_regions.py"
+vcf_fix_header_py="scripts/vcffirstheader.py"
+
+mkdir -p "${dir}/variants_gvcf_transcripts/"
+mkdir -p "${dir}/variants_combined_transcripts/"
+
+# list of input bam
+bam_input_list=$( for s in $sample_list ; do 
+	echo ${dir}/alignments_transcripts/${s}.bam
+done )
+
+# list of input raw vcf
+vcf_input_list=$( for s in $sample_list ; do 
+	echo ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.gz
+done )
+
+
+
+
+
+## FUNCTIONS
+
+# function: sample-spcific genotyping with freebayes
+vcf_freebayes () {
+	# generate regions to analyse in parallel, using GNU-parallel
+	python3 $generate_regions_py ${ref}.fai 1000000 | grep -v "^UNKN" > ${dir}/variants_gvcf_transcripts/regions.list
+   for s in $sample_list ; do
+		if [ ! -f "${dir}/alignments_transcripts/${s}.bam.bai" ] ; then
+			samtools index -@ ${nt} ${dir}/alignments_transcripts/${s}.bam
+		fi
+		if [ ! -f "${dir}/variants_gvcf_transcripts/${s}.raw.vcf.gz" ] ; then
+			echo "$(date '+%Y-%m-%d %H:%M:%S') CREATE RAW VCF: ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.gz"
+			# create raw calls for all samples and all chroms
+			parallel -a ${dir}/variants_gvcf_transcripts/regions.list -j ${nt} -k \
+			freebayes --region {} -f ${ref} ${dir}/alignments_transcripts/${s}.bam --ploidy 10 --pooled-discrete --pooled-continuous \
+			| python2 $vcf_fix_header_py  \
+			| vcfstreamsort -w 1000 \
+			| vcfsnps \
+			| vcfuniq \
+			> ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.tmp1 \
+			&& mv ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.tmp1 ${dir}/variants_gvcf_transcripts/${s}.raw.vcf \
+			&& bgzip ${dir}/variants_gvcf_transcripts/${s}.raw.vcf \
+			&& tabix -p vcf ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.gz
+		else
+			echo "$(date '+%Y-%m-%d %H:%M:%S') FOUND RAW VCF: ${dir}/variants_gvcf_transcripts/${s}.raw.vcf.gz"
+		fi
+	done
+}
+
+
+# combine all samples into a single VCF
+vcf_combine () {
+	if [ ! -f "${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz" ] ; then
+		echo "$(date '+%Y-%m-%d %H:%M:%S') COMBINE RAW VCFs: ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz"
+		for list in transcript_split_bedfiles/list_* ; do
+			bcftools merge --threads ${nt} --regions-file ${list} \
+			${vcf_input_list} --output-type z \
+			--info-rules DP:sum,RO:sum,QR:sum,AO:sum,QA:sum
+		done \
+		> ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.tmp.gz \
+		&& mv ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.tmp.gz ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz \
+		&& tabix -p vcf ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz
+	else
+		echo "$(date '+%Y-%m-%d %H:%M:%S') FOUND RAW VCFs: ${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz"
+	fi
+}
+
+# apply quality filters to the joint VCF
+vcf_filter () {
+	if [ ! -f "${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.gz" ] ; then
+		echo "$(date '+%Y-%m-%d %H:%M:%S') FILTER VCF: ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.gz"
+		bcftools filter \
+		${dir}/variants_combined_transcripts/${out_prefix}_raw.vcf.gz \
+		--include 'QUAL>30 & N_MISSING < 2 & TYPE="snp"' \
+		--output-type z \
+		--output ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.tmp1.gz \
+		&& mv ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.tmp1.gz ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.gz \
+		&& tabix -p vcf ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.gz
+
+	else
+		echo "$(date '+%Y-%m-%d %H:%M:%S') FOUND FILTERED VCF: ${dir}/variants_combined_transcripts/${out_prefix}_recode.vcf.gz"
+	fi
+
+}
+
+## MAIN
+
+vcf_freebayes \
+&& vcf_combine \
+&& vcf_filter
+
+
+echo "Done"
